f9f858998edca183dc71d61366af19281c97807f
Azubi1234 <132048044+Azubi1234@users.noreply.github.com>
2026-09-14 17:36:08 +0300

    Inspect Deathwing Companion squad size structure

--- .github/build/r54_diag_da_sizes.py
+++ .github/build/r54_diag_da_sizes.py
@@ -1,17 +1,20 @@
 import xml.etree.ElementTree as ET
 CNS='http://www.battlescribe.net/schema/catalogueSchema'; C=lambda t:f'{{{CNS}}}{t}'
-cr=ET.parse('Legiones Astartes.cat').getroot(); top=cr.find(C('selectionEntries'))
-for u in list(top):
-    uid=u.get('id','')
-    if not (uid.startswith('r40-da') or uid.startswith('da22-')): continue
-    models=[]
+cr=ET.parse('Legiones Astartes.cat').getroot()
+
+def by_id(i): return next((e for e in cr.iter() if e.get('id')==i),None)
+for uid in ['da22-deathwing-companions','da22-interemptors']:
+    u=by_id(uid)
+    print('\nUNIT',uid, u.get('name') if u is not None else 'MISSING')
+    if u is None: continue
     for s in u.iter(C('selectionEntry')):
-        if s is u or s.get('type')!='model': continue
+        if s is u: continue
         cs=s.find(C('constraints')); con=[]
-        if cs is not None: con=[(c.get('type'),c.get('value'),c.get('scope')) for c in cs.findall(C('constraint')) if c.get('field')=='selections']
+        if cs is not None: con=[(c.get('type'),c.get('value'),c.get('scope'),c.get('field')) for c in cs.findall(C('constraint'))]
         costs=s.find(C('costs')); pts=[]
-        if costs is not None: pts=[c.get('value') for c in costs.findall(C('cost')) if c.get('typeId')=='pts']
-        models.append((s.get('id'),s.get('name'),s.get('defaultAmount'),con,pts))
-    if models:
-        print('\nUNIT',uid,u.get('name'))
-        for m in models: print(' ',m)
+        if costs is not None: pts=[(c.get('typeId'),c.get('value')) for c in costs.findall(C('cost'))]
+        print(' SE',s.get('id'),repr(s.get('name')),s.get('type'),'default',s.get('defaultAmount'),'con',con,'costs',pts)
+    for g in u.iter(C('selectionEntryGroup')):
+        cs=g.find(C('constraints')); con=[]
+        if cs is not None: con=[(c.get('type'),c.get('value'),c.get('scope'),c.get('field')) for c in cs.findall(C('constraint'))]
+        print(' GROUP',g.get('id'),repr(g.get('name')),'con',con)
