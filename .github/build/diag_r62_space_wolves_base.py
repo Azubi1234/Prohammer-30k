b78d636ef836b77dbe90b558743c8357ba62d54d
Azubi1234 <132048044+Azubi1234@users.noreply.github.com>
2026-09-15 13:05:54 +0300

    Inspect Space Wolves Legion and FOC structure

--- .github/build/diag_r62_space_wolves_base.py
+++ .github/build/diag_r62_space_wolves_base.py
@@ -1,36 +1,27 @@
 from pathlib import Path
 import xml.etree.ElementTree as ET
-
-CAT=Path('Legiones Astartes.cat')
-CNS='http://www.battlescribe.net/schema/catalogueSchema'; C=lambda t:f'{{{CNS}}}{t}'
-cr=ET.parse(CAT).getroot()
-
-def by_id(i): return next((e for e in cr.iter() if e.get('id')==i),None)
-def parent_of(node):
-    for p in cr.iter():
-        if node in list(p): return p
-    return None
-
-def dump_node(i):
-    x=by_id(i)
-    print('\nNODE',i,'FOUND',x is not None)
-    if x is None:return
-    print(' TAG',x.tag.split('}')[-1],'NAME',x.get('name'),'TYPE',x.get('type'),'HIDDEN',x.get('hidden'))
-    p=parent_of(x)
-    print(' PARENT',p.tag.split('}')[-1] if p is not None else None,p.get('id') if p is not None else None,p.get('name') if p is not None else None)
-    if p is not None:
-        for ch in list(p):
-            print('   SIB',ch.tag.split('}')[-1],ch.get('id'),ch.get('name'),ch.get('type'),ch.get('hidden'))
-    print(' XML',ET.tostring(x,encoding='unicode')[:8000])
-
-for i in ['legion-v','legion-ws','legion-da','hq-consul-chaplain','hq-consul-librarian','cat-hq']:
-    dump_node(i)
-
-print('\nSPACE WOLVES NAMED OBJECTS')
-for e in cr.iter():
-    if 'space wolves' in (e.get('name') or '').lower() or 'space wolf' in (e.get('name') or '').lower():
-        print(e.tag.split('}')[-1],e.get('id'),e.get('name'),e.get('type'),e.get('hidden'))
-
-print('\nCATEGORY ENTRIES')
-for e in cr.iter(C('categoryEntry')):
+CAT=Path('Legiones Astartes.cat'); GST=Path('Prohammer 30k.gst')
+CNS='http://www.battlescribe.net/schema/catalogueSchema'; GNS='http://www.battlescribe.net/schema/gameSystemSchema'
+C=lambda t:f'{{{CNS}}}{t}'; G=lambda t:f'{{{GNS}}}{t}'
+cr=ET.parse(CAT).getroot(); gr=ET.parse(GST).getroot()
+def by_id(root,i): return next((e for e in root.iter() if e.get('id')==i),None)
+for i in ['legion-vi','r25-consul-vi-wolf-priest-consul','r25-consul-vi-rune-priest-consul']:
+    x=by_id(cr,i); print('\nNODE',i,'FOUND',x is not None)
+    if x is not None: print(ET.tostring(x,encoding='unicode')[:18000])
+print('\nGAME SYSTEM CATEGORY ENTRIES')
+for e in gr.iter(G('categoryEntry')):
     print(e.get('id'),e.get('name'),ET.tostring(e,encoding='unicode')[:3000])
+print('\nCATALOGUE FORCE ENTRIES')
+for f in cr.iter(C('forceEntry')):
+    print('FORCE',f.get('id'),f.get('name')); print(ET.tostring(f,encoding='unicode')[:10000])
+print('\nGAME SYSTEM FORCE ENTRIES')
+for f in gr.iter(G('forceEntry')):
+    print('FORCE',f.get('id'),f.get('name')); print(ET.tostring(f,encoding='unicode')[:10000])
+print('\nPOINT-COST CONDITIONS / MODIFIERS')
+for root,label,ns in [(cr,'CAT',CNS),(gr,'GST',GNS)]:
+    for e in root.iter():
+        attrs=' '.join(f'{k}={v}' for k,v in e.attrib.items())
+        if 'pts' in attrs.lower() or 'point' in attrs.lower():
+            tag=e.tag.split('}')[-1]
+            if tag in ('condition','constraint','modifier','conditionGroup'):
+                print(label,tag,e.attrib)
